f8e660b02af75ca5972fee75e3f43f30546485f5
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-09-11 09:23:26 +0800

    fix: 修复pm2环境变量导入方式

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,19 @@
 module.exports = {
-  apps: [
-    {
-      name: 'orange',
-      port: '3987',
-      exec_mode: 'cluster',
-      instances: 'max',
-      script: './.output/server/index.mjs'
-    }
-  ]
-}
+	apps: [
+		{
+			name: "orange",
+			port: "3987",
+			exec_mode: "cluster",
+			instances: "max",
+			script: "./.output/server/index.mjs",
+			env_production: {
+				NODE_ENV: "production",
+				databaseHost: process.env.DB_HOST,
+				databaseUser: process.env.DB_USER,
+				databasePassword: process.env.DB_PASSWORD,
+				databaseDatabase: process.env.DB_NAME,
+				databaseCharset: process.env.DB_CHARSET,
+			},
+		},
+	],
+};
