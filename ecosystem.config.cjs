27b942256e0c628e4f4abfdff44ffdabe1afe7ea
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-09-11 15:05:49 +0800

    fix: 删除 ecosystem.config.cjs 的错误配置

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 module.exports = {
 	apps: [
 		{
@@ -7,15 +5,7 @@ module.exports = {
 			port: "3987",
 			exec_mode: "cluster",
 			instances: "max",
-			script: "./.output/server/index.mjs",
-			env_production: {
-				NODE_ENV: "production",
-				databaseHost: process.env.DB_HOST,
-				databaseUser: process.env.DB_USER,
-				databasePassword: process.env.DB_PASSWORD,
-				databaseDatabase: process.env.DB_NAME,
-				databaseCharset: process.env.DB_CHARSET,
-			},
+			script: "./.output/server/index.mjs"
 		},
 	],
 };
