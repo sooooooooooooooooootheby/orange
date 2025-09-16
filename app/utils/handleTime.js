771877060088c1021cfc559fd63b81b349d22a51
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-09-16 22:21:38 +0800

    fix: 修复时间格式化函数时区不正确的问题

--- app/utils/handleTime.js
+++ app/utils/handleTime.js
@@ -1,10 +1,19 @@
 export const handleTime = (time) => {
-    const date = new Date(time);
-    const pad = (n) => n.toString().padStart(2, '0');
-    const yyyy = date.getFullYear();
-    const mm = pad(date.getMonth() + 1);
-    const dd = pad(date.getDate());
-    const hh = pad(date.getHours());
-    const min = pad(date.getMinutes());
-    return `${yyyy}/${mm}/${dd} ${hh}:${min}`;
-}
+	const date = new Date(time);
+
+	if (isNaN(date.getTime())) {
+		throw new Error("Invalid date provided");
+	}
+	const utcTime = date.getTime() + date.getTimezoneOffset() * 60000;
+	const beijingTime = new Date(utcTime + 3600000 * 8);
+
+	const pad = (n) => n.toString().padStart(2, "0");
+
+	const yyyy = beijingTime.getUTCFullYear();
+	const mm = pad(beijingTime.getUTCMonth() + 1);
+	const dd = pad(beijingTime.getUTCDate());
+	const hh = pad(beijingTime.getUTCHours());
+	const min = pad(beijingTime.getUTCMinutes());
+
+	return `${yyyy}/${mm}/${dd} ${hh}:${min}`;
+};
