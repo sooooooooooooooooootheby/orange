5bdad28f861ea1277bab66d8da09a29ce101230f
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-09-19 14:00:03 +0800

    feat: 添加开源地址

--- app/app.vue
+++ app/app.vue
@@ -14,7 +14,14 @@
 	</UApp>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+onMounted(() => {
+	console.log(
+		`\n 你在找什么?\n \n %c GITHUB %c https://github.com/sooooooooooooooooootheby/orange \n\n%c 记得给我点个star⭐ ฅ^•ﻌ•^ฅ`,
+		"color: #ffffff; background: #FE9A00; padding:5px 0;", "background: #FFEDD4; padding:5px 0;", "background: #fff;"
+	);
+});
+</script>
 
 <style>
 .page-enter-active,
