d5657879e64d18a92e116f0e683fefe955769fa1
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-11-15 20:51:57 +0800

    feat: 服务器性能页添加 子服 地图

--- app/plugins/socket.client.ts
+++ app/plugins/socket.client.ts
@@ -5,7 +5,12 @@ export default defineNuxtPlugin((nuxtApp) => {
 		path: "/mainserver/socket.io",
 		transports: ["websocket"],
 	});
+	const zfSocket = io("http://e.rainplay.cn:20349/", {
+		path: "",
+		transports: ["websocket"],
+	});
 
 	// 全局提供
 	nuxtApp.provide("socket", socket);
+	nuxtApp.provide("zf", zfSocket);
 });
